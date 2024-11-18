30099b63d121651ede0622c13e78d8af727151e9
neumann.niklas <neumann.niklas98@gmail.com>
2024-11-18 22:00:52 +0100

    complete add-workout logic

--- src/app/features/workouts/add-workout/add-workout.component.ts
+++ src/app/features/workouts/add-workout/add-workout.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Exercise } from '../../../models/exercise.model';
 import { ExerciseStoreService } from '../../../services/exercise-store.service';
+import { WorkoutStoreService } from '../../../services/workout-store.service';
 
 @Component({
   selector: 'app-add-workout',
@@ -14,27 +15,27 @@ import { ExerciseStoreService } from '../../../services/exercise-store.service';
   styleUrl: './add-workout.component.scss'
 })
 export class AddWorkoutComponent {
-  readonly addWorkoutForm: FormGroup = new FormGroup({
+  readonly addWorkoutFormGroup: FormGroup = new FormGroup({
     name: new FormControl<string>('', Validators.required),
     workoutExercises: new FormArray<FormGroup>([])
   });
   readonly exercises$: Observable<Exercise[]>;
   exercises: Exercise[] = [];
 
-  constructor(private readonly exerciseStoreService: ExerciseStoreService) {
+  constructor(
+    private readonly router: Router,
+    private readonly exerciseStoreService: ExerciseStoreService,
+    private readonly workoutStoreService: WorkoutStoreService
+  ) {
     this.exercises$ = this.exerciseStoreService.exercises$;
   }
 
-  get workoutExercises(): FormGroup[] {
-    return (this.addWorkoutForm.get('workoutExercises') as FormArray<FormGroup>).controls;
-  }
-
-  getWorkoutExerciseSetsAt(index: number): FormGroup[] {
-    return (this.workoutExercises[index].get('sets') as FormArray<FormGroup>).controls;
+  get workoutExercisesFormArray(): FormArray<FormGroup> {
+    return this.addWorkoutFormGroup.get('workoutExercises') as FormArray;
   }
 
   addWorkoutExercise(exercise: Exercise): void {
-    this.workoutExercises.push(new FormGroup({
+    this.workoutExercisesFormArray.push(new FormGroup({
       sets: new FormArray<FormGroup>([
         new FormGroup({
           reps: new FormControl<number>(0, Validators.required),
@@ -46,7 +47,46 @@ export class AddWorkoutComponent {
   }
 
   removeWorkoutExerciseAt(index: number): void {
-    (this.addWorkoutForm.get('workoutExercises') as FormArray).removeAt(index);
+    this.workoutExercisesFormArray.removeAt(index);
     this.exercises = this.exercises.filter((_, i: number) => i !== index);
+  }
+
+  getSetsFormArrayAt(index: number): FormArray<FormGroup> {
+    return this.workoutExercisesFormArray.at(index).get('sets') as FormArray;
+  }
+
+  addSetAt(index: number): void {
+    this.getSetsFormArrayAt(index).push(new FormGroup({
+      reps: new FormControl<number>(0, Validators.required),
+      weightsKg: new FormControl<number>(0, Validators.required)
+    }));
+  }
+
+  removeSetAt(i: number, j: number): void {
+    if (this.getSetsFormArrayAt(i).length <= 1) return;
+
+    this.getSetsFormArrayAt(i).removeAt(j);
+  }
+
+  onReset(): void {
+    this.addWorkoutFormGroup.reset();
+    this.workoutExercisesFormArray.clear();
+    this.exercises = [];
+  }
+
+  onSubmit(): void {
+    if (this.addWorkoutFormGroup.invalid) return;
+
+    this.workoutStoreService.addWorkout({
+      name: this.addWorkoutFormGroup.value.name,
+      workoutExercises: this.workoutExercisesFormArray.controls.map(
+        (formGroup: FormGroup, index: number) => {
+          return {
+            exercise: this.exercises[index],
+            sets: formGroup.value.sets
+          };
+        }
+      )
+    }).subscribe({ complete: () => this.router.navigate(['workouts']) });
   }
 }
--- src/app/models/workout-exercise.model.ts
+++ src/app/models/workout-exercise.model.ts
@@ -1,8 +1,6 @@
 import { Exercise } from "./exercise.model";
-import { Workout } from "./workout.model";
 
 export interface WorkoutExercise {
-    readonly workout: Workout;
     readonly exercise: Exercise;
     readonly sets: {
         readonly reps: number;
